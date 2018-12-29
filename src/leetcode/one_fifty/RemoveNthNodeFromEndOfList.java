4083b5fe40705568451cdeba99183fb4c2af9e23
Van Ha Nguyen <vanhaxl@gmail.com>
2018-12-29 12:00:53 -0600

    leetcode

--- src/leetcode/one_fifty/RemoveNthNodeFromEndOfList.java
+++ src/leetcode/one_fifty/RemoveNthNodeFromEndOfList.java
@@ -1,4 +1,34 @@
 package leetcode.one_fifty;
 
+import leetcode.one_fifty.common.ListNode;
+
 public class RemoveNthNodeFromEndOfList {
+    public static void main(String[] args) {
+        ListNode a = new ListNode(1);
+        a.next = new ListNode(2);
+        a.next.next = new ListNode(3);
+        a.next.next.next = new ListNode(4);
+        a.next.next.next.next = new ListNode(5);
+        System.out.println(removeNthFromEnd(a, 2));
+
+    }
+
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+        slow.next = head;
+
+        for(int i = 1; i<= n; i++){
+            fast = fast.next;
+        }
+        while(fast.next != null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+
+    }
+
 }
---
+++ src/leetcode/one_fifty/common/ListNode.java
@@ -0,0 +1,32 @@
+package leetcode.one_fifty.common;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int x) {
+        val = x;
+    }
+
+    public int getSize(){
+        int s = 1;
+        ListNode tmp = this.next;
+        while(tmp != null){
+            s++;
+            tmp = tmp.next;
+        }
+        return s;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.val);
+        ListNode tmp = this.next;
+        while(tmp!= null){
+            sb.append("->");
+            sb.append(tmp.val);
+            tmp = tmp.next;
+        }
+        return sb.toString();
+    }
+}
