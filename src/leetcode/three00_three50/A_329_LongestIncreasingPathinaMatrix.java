86b1c60338ae29d3be6753795a4560c3d97316f1
Van Ha Nguyen <vanhaxl@gmail.com>
2019-01-04 10:54:30 -0600

    letcode

---
+++ src/leetcode/three00_three50/A_329_LongestIncreasingPathinaMatrix.java
@@ -0,0 +1,51 @@
+package leetcode.three00_three50;
+
+class Point {
+    int x;
+    int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class A_329_LongestIncreasingPathinaMatrix {
+    public static void main(String[] args) {
+        int[][] arr = {{9, 9, 4},
+                {6, 6, 8},
+                {2, 1, 1}};
+        System.out.println(longestIncreasingPath(arr));
+    }
+
+    public static int longestIncreasingPath(int[][] arr) {
+        if (arr.length == 0) return 0;
+        int[][] memo = new int[arr.length][arr[0].length];
+
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                max = Math.max(max, helper(arr, i, j, memo));
+            }
+        }
+        return max;
+    }
+
+    public static int helper(int[][] arr, int x, int y, int[][] memo) {
+        if (memo[x][y] != 0) return memo[x][y];
+        if (x + 1 < arr.length && arr[x + 1][y] > arr[x][y]) {
+            memo[x][y] = Math.max(memo[x][y], helper(arr, x + 1, y, memo));
+        }
+        if (x - 1 >= 0 && arr[x - 1][y] > arr[x][y]) {
+            memo[x][y] = Math.max(memo[x][y], helper(arr, x - 1, y, memo));
+        }
+        if (y + 1 < arr[0].length && arr[x][y + 1] > arr[x][y]) {
+            memo[x][y] = Math.max(memo[x][y], helper(arr, x, y + 1, memo));
+        }
+        if (y - 1 >= 0 && arr[x][y - 1] > arr[x][y]) {
+            memo[x][y] = Math.max(memo[x][y], helper(arr, x, y - 1, memo));
+        }
+        return ++memo[x][y];
+
+    }
+}
