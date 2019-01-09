a6f4a9cd46b2499b89924edaf18199e44d9ca2c2
vanhaxl <vanhaxl@gmail.com>
2019-01-09 16:04:42 -0600

    letcode

--- src/leetcode/one_fifty/A_2_AddTwoNumbers.java
+++ src/leetcode/one_fifty/A_2_AddTwoNumbers.java
@@ -4,19 +4,37 @@ import leetcode.common.ListNode;
 
 public class A_2_AddTwoNumbers {
     public static void main(String[] args) {
+        ListNode a = new ListNode(2);
+        a.next = new ListNode(4);
+        a.next.next = new ListNode(3);
 
+        ListNode b = new ListNode(5);
+        b.next = new ListNode(6);
+        b.next.next = new ListNode(4);
+
+        ListNode result = addTwoNumbers(a, b);
+        System.out.println(result);
     }
 
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(-1);
         ListNode tmp = dummy;
-        int value = 0, remember = 0;
-        while (l1 != null && l2 != null) {
-            int sum = (l1.val + l2.val);
+        int value = 0, remember = 0, sum = 0;
+        while (l1 != null || l2 != null) {
+            int a = l1 == null ? 0 : l1.val;
+            int b = l2 == null ? 0 : l2.val;
+            sum = a + b + remember;
             value = sum % 10;
             remember = sum / 10;
-            ListNode tmpNode = new ListNode(value);
-
+            tmp.next = new ListNode(value);
+            tmp = tmp.next;
+            if (l1 != null)
+                l1 = l1.next;
+            if (l2 != null)
+                l2 = l2.next;
+        }
+        if(remember > 0){
+            tmp.next = new ListNode(remember);
         }
         return dummy.next;
     }
