af347655e56fffe5204b6b2c0e20961d8959597f
vanhaxl <vanhaxl@gmail.com>
2019-01-09 08:46:40 -0600

    letcode

--- src/leetcode/one_fifty/A_2_AddTwoNumbers.java
+++ src/leetcode/one_fifty/A_2_AddTwoNumbers.java
@@ -3,10 +3,27 @@ package leetcode.one_fifty;
 import leetcode.common.ListNode;
 
 public class A_2_AddTwoNumbers {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
     }
+
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return null;
+        ListNode dummy = new ListNode(-1);
+        ListNode tmp = dummy;
+        int value = 0, remember = 0;
+        while (l1 != null && l2 != null) {
+            int sum = (l1.val + l2.val);
+            value = sum % 10;
+            remember = sum / 10;
+            ListNode tmpNode = new ListNode(value);
+
+        }
+        return dummy.next;
     }
 }
+
+/*
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+ */
