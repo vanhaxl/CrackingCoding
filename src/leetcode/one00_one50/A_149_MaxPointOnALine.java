0bec4494a6147b1442cfbaeca765251847a288e6
vanhaxl <vanhaxl@gmail.com>
2019-01-14 13:56:16 -0600

    letcode

---
+++ src/leetcode/one00_one50/A_149_MaxPointOnALine.java
@@ -0,0 +1,19 @@
+package leetcode.one00_one50;
+
+import leetcode.common.Point;
+
+public class A_149_MaxPointOnALine {
+    public static void main(String[] args) {
+
+    }
+
+    public static int maxPoints(Point[] points) {
+        return 1;
+    }
+
+
+}
+
+/*
+Given n points on a 2D plane, find the maximum number of points that lie on the same straight line.
+ */
