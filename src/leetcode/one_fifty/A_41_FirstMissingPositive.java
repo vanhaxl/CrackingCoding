2924e7aab469ba3ab773355033f57593e85649ad
vanhaxl <vanhaxl@gmail.com>
2019-01-14 09:52:43 -0600

    l

---
+++ src/leetcode/one_fifty/A_41_FirstMissingPositive.java
@@ -0,0 +1,34 @@
+package leetcode.one_fifty;
+
+public class A_41_FirstMissingPositive {
+    public static void main(String[] args) {
+        int[] nums = {3, 4, -1, 1};
+        System.out.println(firstMissingPositive(nums));
+    }
+
+    public static int firstMissingPositive(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            if (nums[i] >= 1 && nums[i] <= nums.length && nums[nums[i] - 1] != nums[i]) {
+                swap(nums, i, nums[i] - 1);
+            } else {
+                i++;
+            }
+        }
+        i = 0;
+        while (i < nums.length && nums[i] == i + 1) i++;
+        return i + 1;
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
+
+/*
+- a valid integer should be >= 1 and <= len
+- if we see a valid integer, swap it to its correct position if position is not correct
+- use loop to check if nums[i] != i+1 return i+1 which is the smallest number missing
+ */
