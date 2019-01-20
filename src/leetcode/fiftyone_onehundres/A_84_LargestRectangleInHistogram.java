62eeb2791775111bdf82d4f8a675bc33c49e12fc
vanhaxl <vanhaxl@gmail.com>
2019-01-20 14:29:04 -0600

    letcode

--- src/leetcode/fiftyone_onehundres/A_84_LargestRectangleInHistogram.java
+++ src/leetcode/fiftyone_onehundres/A_84_LargestRectangleInHistogram.java
@@ -14,13 +14,13 @@ public class A_84_LargestRectangleInHistogram {
         int maxarea = 0;
         for (int i = 0; i < heights.length; i++) {
             while (stack.peek() != -1 && heights[stack.peek()] >= heights[i]) {
-                maxarea = Math.max(maxarea, heights[stack.pop()] * (i - stack.peek() - 1));
+                maxarea = Math.max(maxarea, heights[stack.pop()] * (i - stack.peek() - 1));// vi du i = 4, 4-2 -1
             }
             stack.push(i);
         }
         while (stack.peek() != -1) {
-            maxarea = Math.max(maxarea, heights[stack.pop()] * (heights.length - stack.peek() - 1));
-        }
+            maxarea = Math.max(maxarea, heights[stack.pop()] * (heights.length - stack.peek() - 1)); // tuong tu len
+        }                                                                               // giong h[len] = -(vo^ cu'ng)
         return maxarea;
     }
 }
