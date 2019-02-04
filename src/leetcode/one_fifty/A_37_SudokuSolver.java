a6afd47259ea45c72b2962b5b403676522fdf4ab
vanhaxl <vanhaxl@gmail.com>
2019-02-04 13:48:39 -0600

    letcode

--- src/leetcode/one_fifty/A_37_SudokuSolver.java
+++ src/leetcode/one_fifty/A_37_SudokuSolver.java
@@ -1,12 +1,76 @@
 package leetcode.one_fifty;
 
+import java.util.Arrays;
+
 public class A_37_SudokuSolver {
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
+        char[][] board = {
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+        solveSudoku(board);
+        for(char[] b: board){
+            System.out.println(Arrays.toString(b));
+        }
     }
-    public static void solveSudoku(char[][] board) {
-        //get all available for each row
-        //get all availabe for each col
 
+    public static void solveSudoku(char[][] board) {
+        if (board == null || board.length == 0)
+            return;
+        solve(board);
+    }
+
+    public static boolean solve(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == '.') {
+                    for (char c = '1'; c <= '9'; c++) {//trial. Try 1 through 9
+                        if (isValid(board, i, j, c)) {
+                            board[i][j] = c; //Put c for this cell
+
+                            if (solve(board))
+                                return true; //If it's the solution return true
+                            else
+                                board[i][j] = '.'; //Otherwise go back
+                        }
+                    }
+
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static boolean isValid(char[][] board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] != '.' && board[i][col] == c) return false; //check row
+            if (board[row][i] != '.' && board[row][i] == c) return false; //check column
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] != '.' &&
+                    board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) return false; //check 3*3 block
+        }
+        return true;
     }
 }
+
+/*
+A sudoku solution must satisfy all of the following rules:
+
+Each of the digits 1-9 must occur exactly once in each row.
+Each of the digits 1-9 must occur exactly once in each column.
+Each of the the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+Empty cells are indicated by the character '.'.
+ */
+
+/*
+Try 1 through 9 for each cell. The time complexity should be 9 ^ m
+(m represents the number of blanks to be filled in), since each
+blank can have 9 choices. Details see comments inside code.
+ */
---
+++ src/leetcode/one_fifty/A_51_NQueens.java
@@ -0,0 +1,82 @@
+package leetcode.one_fifty;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class A_51_NQueens {
+    public static void main(String[] args) {
+        List<List<String>> list = solveNQueens(4);
+        for (List<String> l : list) {
+            for (String s : l) {
+                System.out.println(s);
+            }
+            System.out.println();
+            System.out.println();
+        }
+    }
+
+    public static List<List<String>> solveNQueens(int n) {
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+        List<List<String>> res = new ArrayList<>();
+        dfs(board, 0, res);
+        return res;
+    }
+
+    public static void dfs(char[][] board, int colIndex, List<List<String>> res) {
+        if (colIndex == board.length) { //it is colIndex == board[0].length, but board.length == board[0].length = n
+            res.add(construct(board));
+            return;
+        }
+        for (int i = 0; i < board.length; i++) { // loop from top to bottom
+            if (validate(board, i, colIndex)) {
+                board[i][colIndex] = 'Q';
+                dfs(board, colIndex + 1, res);
+                board[i][colIndex] = '.';
+            }
+        }
+    }
+
+    public static boolean validate(char[][] board, int x, int y) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < y; j++) {
+                if (board[i][j] == 'Q' && (x + j == y + i || x + y == i + j || x == i)) {
+                    // x+j == y+i: diagonal \
+                    // x+y == i+j: diagonal /
+                    // x == i: same line
+                    // no need to check column, as backtracking, reverse from 'Q' to '.'
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static List<String> construct(char[][] board) {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            String s = new String(board[i]);
+            list.add(s);
+        }
+        return list;
+    }
+}
+
+/*
+Input: 4
+Output: [
+ [".Q..",  // Solution 1
+  "...Q",
+  "Q...",
+  "..Q."],
+
+ ["..Q.",  // Solution 2
+  "Q...",
+  "...Q",
+  ".Q.."]
+]
+ */
