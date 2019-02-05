0a09f16c950efec52bd0fc1c4e682a88940844a9
vanhaxl <vanhaxl@gmail.com>
2019-02-05 08:51:34 -0600

    letcode

--- src/leetcode/one_fifty/A_37_SudokuSolver.java
+++ src/leetcode/one_fifty/A_37_SudokuSolver.java
@@ -16,7 +16,7 @@ public class A_37_SudokuSolver {
                 {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
         };
         solveSudoku(board);
-        for(char[] b: board){
+        for (char[] b : board) {
             System.out.println(Arrays.toString(b));
         }
     }
@@ -28,11 +28,11 @@ public class A_37_SudokuSolver {
     }
 
     public static boolean solve(char[][] board) {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
+        for (int i = 0; i < board.length; i++) { // top down
+            for (int j = 0; j < board[0].length; j++) { // left right
                 if (board[i][j] == '.') {
                     for (char c = '1'; c <= '9'; c++) {//trial. Try 1 through 9
-                        if (isValid(board, i, j, c)) {
+                        if (isValid(board, i, j, c)) {  // check if it is valid if we fill in c into position i, j
                             board[i][j] = c; //Put c for this cell
 
                             if (solve(board))
@@ -42,13 +42,14 @@ public class A_37_SudokuSolver {
                         }
                     }
 
-                    return false;
+                    return false; // there is no option for this cell, so return false
                 }
             }
         }
         return true;
     }
 
+    // check if it is valid if we fill in c into position i, j
     private static boolean isValid(char[][] board, int row, int col, char c) {
         for (int i = 0; i < 9; i++) {
             if (board[i][col] != '.' && board[i][col] == c) return false; //check row
