2fe79c272762ad85fab467b89fb6f0aca2e330e2
vanhaxl <vanhaxl@gmail.com>
2019-02-07 09:47:11 -0600

    letcode

---
+++ src/leetcode/fiftyone_onehundres/A_85_MaximalRectangle.java
@@ -0,0 +1,35 @@
+package leetcode.fiftyone_onehundres;
+
+public class A_85_MaximalRectangle {
+    public static  void main(String[] args){
+
+    }
+
+    public static int maximalRectangle(char[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] dp = new int[m+1][n+1];
+        for(int i = 1; i<= m; i++){
+            for(int j = 1; j<=n; j++){
+                dp[i][j] =
+            }
+        }
+
+    }
+}
+
+/*
+Input:
+[
+  ["1","0","1","0","0"],
+  ["1","0","1","1","1"],
+  ["1","1","1","1","1"],
+  ["1","0","0","1","0"]
+]
+Output: 6
+ */
+
+/*
+first row: 0
+first col: 0
+ */
