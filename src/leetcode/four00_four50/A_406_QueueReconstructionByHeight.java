e37c1cb3fe071a61d8673eda2c81ed39bf59041e
vanhaxl <vanhaxl@gmail.com>
2019-02-18 08:55:20 -0600

    code

---
+++ src/leetcode/four00_four50/A_406_QueueReconstructionByHeight.java
@@ -0,0 +1,22 @@
+package leetcode.four00_four50;
+
+public class A_406_QueueReconstructionByHeight {
+	public static void main(String[] args) {
+		
+	}
+	
+	public static int[][] reconstructQueue(int[][] people){
+		return null;
+	}
+
+}
+
+/*
+Example
+
+Input:
+[[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+
+Output:
+[[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
+*/
