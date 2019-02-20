01a811785d54f7e06b2111198d63f404782f93d5
vanhaxl <vanhaxl@gmail.com>
2019-02-20 15:15:47 -0600

    code

--- src/leetcode/two51_two99/A_253_MeetingRoomII.java
+++ src/leetcode/two51_two99/A_253_MeetingRoomII.java
@@ -1,27 +1,53 @@
 package leetcode.two51_two99;
 
 
-class Interval{
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Interval {
     int start;
     int end;
 
-    public Interval(){
+    public Interval() {
         start = 0;
         end = 0;
     }
 
-    public Interval(int start, int end){
+    public Interval(int start, int end) {
         this.start = start;
         this.end = end;
     }
 }
-public class A_253_MeetingRoomII {
-    public static  void main(String[] args){
 
+public class A_253_MeetingRoomII {
+    public static void main(String[] args) {
+        Interval a = new Interval(9, 10);
+        Interval b = new Interval(4, 9);
+        Interval c = new Interval(4, 17);
+        Interval[] intervals = {a, b, c};
+        System.out.println(minMeetingRooms(intervals));
     }
 
-    public static  int minMeetingRooms(Interval[] intervals) {
-        return 0;
+    //til todo
+    public static int minMeetingRooms(Interval[] intervals) {
+        if (intervals.length == 0) return 0;
+        Comparator<Interval> comparator = new Comparator<Interval>() {
+            @Override
+            public int compare(Interval interval1, Interval interval2) {
+                if (interval1.end == interval2.end) {
+                    return interval1.start - interval2.start;
+                }
+                return interval1.end - interval2.end;
+            }
+        };
+        Arrays.sort(intervals, comparator);
+        int count = 1;
+        for (int i = 0; i < intervals.length - 1; i++) {
+            if (intervals[i + 1].start < intervals[i].end) {
+                count++;
+            }
+        }
+        return count;
     }
 
 
@@ -32,15 +58,37 @@ Given an array of meeting time intervals consisting of start and end times [[s1,
 find the minimum number of conference rooms required.
 
 Example 1:
-Input: [[0, 30],[5, 10],[15, 20]]  // 2 conflict, 1 ko conflict
+Input: [[0, 30], [5, 10],[15, 20]]  // 2 conflict, 1 ko conflict
 Output: 2
+
+[5, 10], [15, 20], [0, 30]
 
 Example 2:
 Input: [[7,10],[2,4]]// no conflict -> 1
 Output: 1
+
+[2,4], [7,10]
  */
 /*
-0 conflic -> 1
+0 conflict -> 1
 1 conflict -> 2
-2 conflict ->
+2 conflict -> 2
+ */
+
+/*
+//beat 5% O(n^2)
+ public static int minMeetingRooms(Interval[] intervals) {
+        int maxConflict = 0;
+        int tmpCount;
+        for (Interval x: intervals) {
+            tmpCount = 0;
+            for (Interval interval : intervals) {
+                if (interval.start <= x.start && interval.end > x.start) {
+                    tmpCount++;
+                }
+            }
+            maxConflict = Math.max(maxConflict, tmpCount);
+        }
+        return maxConflict;
+    }
  */
