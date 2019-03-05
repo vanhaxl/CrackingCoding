373f7d637ce3b8e6452d7fc904283f278a39662c
vanhaxl <vanhaxl@gmail.com>
2019-03-05 13:31:45 -0600

    code

---
+++ src/leetcode/eight51_eight99/A_877_StoneGame.java
@@ -0,0 +1,12 @@
+package leetcode.eight51_eight99;
+
+public class A_877_StoneGame {
+    public static void main(String[] args) {
+        int[] piles = {5,3,4,5};
+        System.out.println(stoneGame(piles));
+    }
+
+    public static boolean stoneGame(int[] piles) {
+        return false;
+    }
+}
--- src/leetcode/two51_two99/A_276_PaintFence.java
+++ src/leetcode/two51_two99/A_276_PaintFence.java
@@ -2,12 +2,12 @@ package leetcode.two51_two99;
 
 public class A_276_PaintFence {
     public static void main(String[] args) {
-        System.out.println(numWays(3, 2));
+        System.out.println(numWays(3, 0));
     }
 
     public static int numWays(int n, int k) {
         if (n <= 0) return 0;
-        int same = 0, notSame = k;
+        int same = 0, notSame = k; // co 1 thang thi same = 0, vi ko co thang nao dang truoc, not same =k
         for (int i = 1; i < n; i++) {
             int temp = same;
             same = notSame; //ok. same se = previous notSame, vi du aab -> aabb: phai same, nen se bang previous notS
