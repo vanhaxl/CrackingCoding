d0faac537d1add6afa517a5d9acdb4d78ba60c59
vanhaxl <vanhaxl@gmail.com>
2019-03-08 10:39:11 -0600

    code

--- src/leetcode/fiftyone_onehundres/A_72_EditDistance.java
+++ src/leetcode/fiftyone_onehundres/A_72_EditDistance.java
@@ -1,12 +1,45 @@
 package leetcode.fiftyone_onehundres;
 
+import java.util.Arrays;
+
 public class A_72_EditDistance {
     public static void main(String[] args) {
-
+        String word1 = "horse";
+        String word2 = "ros";
+        System.out.println(minDistance(word1, word2));
     }
 
     public static int minDistance(String word1, String word2) {
-        return 0;
+        int m = word1.length();
+        int n = word2.length();
+        if (m * n == 0) return m + n;
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 0; i <= n; i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 0; i <= m; i++) {
+            System.out.println(Arrays.toString(dp[i]));
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                char c1 = word1.charAt(i - 1);
+                char c2 = word2.charAt(j - 1);
+                if (c1 == c2) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    int a = dp[i - 1][j];
+                    int b = dp[i][j - 1];
+                    int c = dp[i - 1][j - 1];
+                    dp[i][j] = a < b ? (a < c ? a : c) : (b < c ? b : c);
+                    dp[i][j]++;
+                }
+            }
+        }
+        return dp[m][n];
     }
 }
 
@@ -30,8 +63,8 @@ rose -> ros (remove 'e')
 
 /*
 horse
-h: keep
-h: del
-h: insert
-h: replace
+h: keep khi giong nhau keep: dp[i][j] = dp[i-1][j-1]
+h: del : dp[i][j] = dp[i-1][j] +1
+h: insert dp[i][j] = dp[i][j-1] + 1
+h: replace dp[i][j] = dp[i-1][j-1] + 1
  */
--- src/leetcode/nine51_nine99/A_983_MinimumCostForTicket.java
+++ src/leetcode/nine51_nine99/A_983_MinimumCostForTicket.java
@@ -1,28 +1,52 @@
 package leetcode.nine51_nine99;
 
 public class A_983_MinimumCostForTicket {
-    public static  void main(String[] args){
 
+    public static void main(String[] args) {
+        int[] days = {1,2,3,4,5,6,7,8,9,10,30,31};
+        int[] costs = {2, 7, 15};
+        System.out.println(mincostTickets(days, costs));
     }
+
     public static int mincostTickets(int[] days, int[] costs) {
-        return 0;
+        int n = days.length;
+        int[] dp = new int[n];
+        dp[0] = costs[0];
+        for (int i = 1; i < n; i++) {
+            int index1 = i;
+            int index2 = i;
+            while (index1 > 0 && days[index1] > days[i] - 7 && days[index1 - 1] + 7 > days[i]) {
+                index1--;
+            }
+            while (index2 > 0 && days[index2] > days[i] - 30 && days[index2 - 1] + 30 > days[i]) {
+                index2--;
+            }
+            dp[i] = Math.min(dp[i - 1] + costs[0], Math.min(index1 == 0 ? costs[1] : (dp[index1 - 1] + costs[1]), (index2 == 0 ? costs[2] : dp[index2 - 1] + costs[2])));
+        }
+        return dp[n - 1];
     }
 
 }
 
 /*
-In a country popular for train travel, you have planned some train travelling one year in advance.  The days of the year that you will travel is given as an array days.  Each day is an integer from 1 to 365.
+In a country popular for train travel, you have planned some train travelling one year in advance.
+The days of the year that you will travel is given as an array days.  Each day is an integer from 1 to 365.
 
 Train tickets are sold in 3 different ways:
 
 a 1-day pass is sold for costs[0] dollars;
 a 7-day pass is sold for costs[1] dollars;
 a 30-day pass is sold for costs[2] dollars.
-The passes allow that many days of consecutive travel.  For example, if we get a 7-day pass on day 2, then we can travel for 7 days: day 2, 3, 4, 5, 6, 7, and 8.
+The passes allow that many days of consecutive travel.
+For example, if we get a 7-day pass on day 2,
+then we can travel for 7 days: day 2, 3, 4, 5, 6, 7, and 8.
 
 Return the minimum number of dollars you need to travel every day in the given list of days.
 
-
+dp[i]: mincost till days[i-1]
+result: dp[n];
+index nao ma arr[index1] >  arr[i] - 7, arr[index2] > arr[i] - 30
+dp[i] = min(dp[i-1] + cost[0], dp[index1-1] + cost[1], dp[index2-1] + cost[2])
 
 Example 1:
 
