02791230eae7cc14aa0e08b451bdfc8d3bf0ef49
vanhaxl <vn505y8@homeoffice.wal-mart.com>
2019-06-07 09:57:54 -0500

    rename

--- src/leetcode/fiftyone_onehundres/A_54_SpiralMatrix.java
+++ src/leetcode/fiftyone_onehundres/A_54_SpiralMatrix.java
@@ -9,6 +9,8 @@ public class A_54_SpiralMatrix {
 		System.out.println(spiralOrder(arr));
 	}
 
+
+
 	public static List<Integer> spiralOrder(int[][] arr) {
 		List<Integer> list = new ArrayList<>();
 		if (arr == null || arr.length == 0 || arr[0].length == 0)
@@ -17,27 +19,27 @@ public class A_54_SpiralMatrix {
 		// l start col
 		// m row
 		// n cols
-		int k = 0, l = 0, m = arr.length, n = arr[0].length;
-		while (k < m && l < n) {
-			for (int i = l; i < n; i++) { // go right
-				list.add(arr[k][i]);
+		int r = 0, c = 0, m = arr.length, n = arr[0].length;
+		while (r < m && c < n) {
+			for (int i = c; i < n; i++) { // go right
+				list.add(arr[r][i]);
 			}
-			k++;
-			for (int i = k; i < m; i++) { // go down
+			r++;
+			for (int i = r; i < m; i++) { // go down
 				list.add(arr[i][n - 1]);
 			}
 			n--;
-			if (k < m) {
-				for (int i = n - 1; i >= l; i--) { // go left
+			if (r < m) {
+				for (int i = n - 1; i >= c; i--) { // go left
 					list.add(arr[m - 1][i]);
 				}
 				m--;
 			}
-			if (l < n) {
-				for (int i = m - 1; i >= k; i--) { // go up
-					list.add(arr[i][l]);
+			if (c < n) {
+				for (int i = m - 1; i >= r; i--) { // go up
+					list.add(arr[i][c]);
 				}
-				l++;
+				c++;
 			}
 		}
 		return list;
